cf002f9e1a256bd7efc75fc3e0d828b99c572d63
Jessica Kerr <jessitron@honeycomb.io>
2024-01-15 16:17:36 -0600

    I can add an attribute to the span

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.opentelemetry.api.trace.Span;
+
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
+     Span span = Span.current();
+     span.setAttribute("jess.was.here",true);
 		return "Greetings from Spring Boot!";
 	}
 
