5f2e1441b0c4f840ff41a1c3f24bf6b168680a2f
Jessica Kerr <jessitron@gmail.com>
2024-01-17 16:42:01 -0600

    Don't add the thread count, let the spanProcessor do that

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -11,27 +11,8 @@ public class HelloController {
 	@GetMapping("/")
 	public String index() {
     Span span = Span.current();
-    span.setAttribute("jess.was.here",true);
-    addGlobalThreadCountToSpan();
+    span.setAttribute("jess.was.here", true);
 		return "Greetings from Spring Boot!";
 	}
-
-  private void addGlobalThreadCountToSpan() {
-    Span span = Span.current();
-    // Get the current thread's thread group
-    ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
-    span.setAttribute("thread.group.name", rootGroup.getName());
-    ThreadGroup parentGroup;
-    while ((parentGroup = rootGroup.getParent()) != null) {
-        rootGroup = parentGroup;
-    }
-
-    // Active count of threads in the thread group
-    int activeThreads = rootGroup.activeCount();
-    span.setAttribute("thread.root.active.count", activeThreads);
-
-    System.out.println("Number of active threads: " + activeThreads);
-
-  }
 
 }
